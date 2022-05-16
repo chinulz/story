cfad19ab6c336dc152b8766af8cdcfe97e883e89
chinulz <chinulkenul117@gmail.com>
2022-05-17 01:29:24 +0700

    ChinuLz

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 10,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    /*loop: true,*/
+});
